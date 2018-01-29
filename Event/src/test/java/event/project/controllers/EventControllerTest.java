e7d37003cbc1549b8b20aa676122c659cb9efc7e
malaysia21 <agnieszka.puzia@gmail.com>
2018-01-29 23:12:50 +0100

    mvcmock

--- Event/src/test/java/event/project/controllers/EventControllerTest.java
+++ Event/src/test/java/event/project/controllers/EventControllerTest.java
@@ -20,6 +20,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -40,7 +41,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(EventController.class)
+@WebMvcTest
+@Configuration
 public class EventControllerTest {
 
     @Autowired
